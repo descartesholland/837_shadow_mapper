c325c419346d429299989acd6a4015c0ef8e8563
Descartes A Holland <hollandd@mac-56-129-09.mit.edu>
2017-11-20 02:17:08 -0500

    Added code to repository

---
+++ src/main.cpp
@@ -0,0 +1,239 @@
+#include "gl.h"
+#include <GLFW/glfw3.h>
+
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <vector>
+#include <lodepng.h>
+#include <map>
+#include <cstdint>
+
+#include "objparser.h"
+
+// some utility code is tucked away in main.h
+// for example, drawing the coordinate axes
+// or helpers for setting uniforms.
+#include "main.h"
+
+#include "iostream"
+
+using namespace std;
+// 4096x4096 is a pretty large texture. Extensions to shadow algorithm
+// (extra credit) help lowering this memory footprint.
+const int SHADOW_WIDTH = 4096;
+const int SHADOW_HEIGHT = 4096;
+
+// FUNCTION DECLARATIONS - you will implement these
+void loadTextures();
+void freeTextures();
+
+void loadFramebuffer();
+void freeFramebuffer();
+
+void draw();
+
+Matrix4f getLightView();
+Matrix4f getLightProjection();
+
+// Globals here.
+objparser scene;
+Vector3f  light_dir;
+glfwtimer timer;
+VertexRecorder rec;
+std::map<std::string, GLuint> glTextures;
+
+
+// FUNCTION IMPLEMENTATIONS
+
+// animate light source direction
+// this one is implemented for you
+void updateLightDirection() {
+    // feel free to edit this
+    float elapsed_s = timer.elapsed();
+    //elapsed_s = 88.88f;
+    float timescale = 0.1f;
+    light_dir = Vector3f(2.0f * sinf((float)elapsed_s * 1.5f * timescale),
+        5.0f, 2.0f * cosf(2 + 1.9f * (float)elapsed_s * timescale));
+    light_dir.normalize();
+}
+
+
+void drawScene(GLint program, Matrix4f V, Matrix4f P) {
+
+    Matrix4f M = Matrix4f::identity();
+    updateTransformUniforms( program, M, V, P);
+    
+    int i = 0;
+    for(draw_batch batch : scene.batches) {
+        int ii = 0;
+        for (int ii = batch.start_index; ii < batch.start_index + batch.nindices; ii++) {
+            int currentBatchIndex = scene.indices[ii];
+//            Vector3f position(scene.positions[currentBatchIndex]);
+//            Vector3f normal(scene.normals[currentBatchIndex]);
+//            Vector2f color = scene.texcoords[currentBatchIndex];
+            rec.record(
+                       scene.positions[currentBatchIndex],
+                       scene.normals[currentBatchIndex],
+                       Vector3f(scene.texcoords[currentBatchIndex][0], scene.texcoords[currentBatchIndex][1], 0));
+            
+        }
+        
+        updateMaterialUniforms( program, batch.mat.diffuse, batch.mat.ambient, batch.mat.specular, batch.mat.shininess);
+        rec.draw();
+        
+        i++;
+    }
+    
+    rec.clear();
+}
+
+void draw() {
+    // 2. DEPTH PASS
+    // - bind framebuffer
+    // - configure viewport
+    // - compute camera matrices (light source as camera)
+    // - call drawScene
+
+    // 1. LIGHT PASS
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    int winw, winh;
+    glfwGetFramebufferSize(window, &winw, &winh);
+    glViewport(0, 0, winw, winh);
+    glUseProgram(program_light);
+    updateLightUniforms(program_light, light_dir, Vector3f(1.2f, 1.2f, 1.2f));
+
+    drawScene(program_light, camera.GetViewMatrix(), camera.GetPerspective());
+
+    // 3. DRAW DEPTH TEXTURE AS QUAD
+    // drawTexturedQuad() helper in main.h is useful here.
+}
+
+void loadTextures() {
+    
+    for(auto it = scene.textures.begin(); it != scene.textures.end(); ++it) {
+        
+        GLuint glTexture;
+        
+        std::string name = it->first;
+        rgbimage& im = it->second;
+        
+        glGenTextures(1, &glTexture);
+        glBindTexture(GL_TEXTURE_2D, glTexture);
+        
+        // Allocate storage for texture; upload pixel data
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, im.data.data());
+        
+        // Enable BiLinear Filtering
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        
+        // Store texture
+        glTextures.insert(std::make_pair(name, glTexture));
+        
+    }
+}
+
+void freeTextures() {
+    for( auto it = glTextures.begin(); it != glTextures.end(); ++it) {
+        std::string name = it->first;
+        GLuint glTexture = it->second;
+        
+        glDeleteTextures(1, &glTexture);
+    }
+    glTextures.clear();
+}
+
+void loadFramebuffer() {
+    
+}
+
+// Main routine.
+// Set up OpenGL, define the callbacks and start the main loop
+int main(int argc, char* argv[])
+{
+    std::string basepath = "./";
+    if (argc > 2) {
+        printf("Usage: %s [basepath]\n", argv[0]);
+    }
+    else if (argc == 2) {
+        basepath = argv[1];
+    }
+    printf("Loading scene and shaders relative to path %s\n", basepath.c_str());
+
+    // load scene data
+    // parsing code is in objparser.cpp
+    // take a look at the public interface in objparser.h
+    if (!scene.parse(basepath + "data/sponza_low/sponza_norm.obj")) {
+        return -1;
+    }
+    
+    rec = VertexRecorder();
+
+    window = createOpenGLWindow(1024, 1024, "Assignment 5");
+
+    // setup the event handlers
+    // key handlers are defined in main.h
+    // take a look at main.h to know what's in there.
+    glfwSetKeyCallback(window, keyCallback);
+    glfwSetMouseButtonCallback(window, mouseCallback);
+    glfwSetCursorPosCallback(window, motionCallback);
+
+    glClearColor(0.8f, 0.8f, 1.0f, 1);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+    loadTextures();
+//    loadFramebuffer();
+
+    camera.SetDimensions(600, 600);
+    camera.SetPerspective(50);
+    camera.SetDistance(10);
+    camera.SetCenter(Vector3f(0, 1, 0));
+    camera.SetRotation(Matrix4f::rotateY(1.6f) * Matrix4f::rotateZ(0.4f));
+
+    // set timer for animations
+    timer.set();
+    while (!glfwWindowShouldClose(window)) {
+        setViewportWindow(window);
+
+        // we reload the shader files each frame.
+        // this shaders can be edited while the program is running
+        // loadPrograms/freePrograms is implemented in main.h
+        bool valid_shaders = loadPrograms(basepath);
+        if (valid_shaders) {
+
+            // draw coordinate axes
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            if (gMousePressed) {
+                drawAxis();
+            }
+
+            // update animation
+            updateLightDirection();
+
+            // draw everything
+            draw();
+        }
+        // make sure to release the shader programs.
+        freePrograms();
+
+        // Make back buffer visible
+        glfwSwapBuffers(window);
+
+        // Check if any input happened during the last frame
+        glfwPollEvents();
+    } // END OF MAIN LOOP
+
+    // All OpenGL resource that are created with
+    // glGen* or glCreate* must be freed.
+    // TODO: add freeXYZ() function calls here
+    // freeFramebuffer();
+    freeTextures();
+
+    glfwDestroyWindow(window);
+
+
+    return 0;	// This line is never reached.
+}
